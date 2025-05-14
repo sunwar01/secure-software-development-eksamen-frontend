26f7942607b100dc5951e19db8f9af3e5ba7a83a
sw_cheats 1 <suwa261101@gmail.com>
2025-05-14 12:30:22 +0200

    AOT enabled. Login component finished. CSP enabled.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {VaultComponent} from './pages/vault/vault.component';
+import {LoginComponent} from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: VaultComponent },
+  { path: 'login', component: LoginComponent },
+
+
+
+];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    ReactiveFormsModule,
+    NgIf,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20), Validators.pattern('^[a-zA-Z0-9_-]+$')]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(50), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*?_])[A-Za-z\\d!@#$%^&*?_]+$')]]
+
+
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid)
+      console.log('Form submitted:', this.loginForm.value);
+    else
+      console.log('Form is invalid');
+  }
+}
